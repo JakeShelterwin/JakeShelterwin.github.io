27d570ff298a53b1650e9ea4bf7bd6c41183e569
Giacomo Suffredini <kirtash.k@live.it>
2022-07-09 19:00:39 +0200

    Nuova Versione

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
-console.log("Io sono la Console");
+console.log("collegato");
+
+$( document ).ready(function() {
+  // il menu rimane fisso, allo scorrere della pagina viene colorato per restare visibile
+  window.onscroll = function() {
+    mostraHeaderFisso();
+    mostraTopScroller();
+    
+};
+
+  function mostraHeaderFisso() {
+      if (document.body.scrollTop > 85 || document.documentElement.scrollTop > 85) {
+          if ($(window).width() < 992) {
+              $("header").addClass("scrollato");
+              $(".fixedHeader").addClass("scrollato");
+            }
+        } else {
+            $("header").removeClass("scrollato");
+            $(".fixedHeader").removeClass("scrollato");
+            $('.mostraContattiMobile, .contattiHeader').removeClass("cliccato");
+        }
+    }
+  function mostraTopScroller() {
+        var winH = $(window).height() / 2;
+        console.log(winH);
+        if (document.body.scrollTop > winH || document.documentElement.scrollTop > winH){
+            $("footer").addClass("scrollato");
+        } else {
+            $("footer").removeClass("scrollato");
+            
+        }
+    }
+
+    $('.mostraContattiMobile').on("click", function(){
+        $(this).toggleClass("cliccato");
+        $('.contattiHeader').toggleClass("cliccato");
+    })
+
+
+
+    // Add smooth scrolling to all links
+    $("a").on('click', function(e) {
+        console.log("ciaociao");
+        /// 1
+        e.preventDefault();
+        // 2
+        const href = $(this).attr("href");
+        // 3
+        $("html, body").animate({ scrollTop: $(href).offset().top }, 800);
+    });
+});
